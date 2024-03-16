c5b37befb8421f383c28745b16f07441f45eb271
Focke Adrian <adrian.focke@extern.a1.at>
2024-03-16 19:09:40 +0100

    backend init changes

--- tina/config.ts
+++ tina/config.ts
@@ -5,10 +5,16 @@ import files from './collections/files';
 import people from './collections/people';
 import standards from './collections/standards';
 
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  '';
+
 export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID ?? '',
   token: process.env.TINA_TOKEN ?? '',
-  branch: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ?? '',
+  branch,
   media: {
     tina: {
       publicFolder: 'public',

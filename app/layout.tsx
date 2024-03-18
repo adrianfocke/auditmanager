f888d193bb9d1a50d6838c5542c969446d0eaeaa
Focke Adrian <adrian.focke@extern.a1.at>
2024-03-18 12:08:46 +0100

    Revert "try to get client from context (where is the problem???)"
    This reverts commit 99b5b756ebecd817467c286621918cf99f763cb9.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import { createContext } from 'react';
 import '../styles/global.css';
-import client from '../tina/__generated__/client';
-
-export const TinaClientContext = createContext<any>(null);
 
 export default function RootLayout({
   children,
@@ -16,9 +12,7 @@ export default function RootLayout({
       <body className='bg-gray-50'>
         <Theme>
           <Container className='p-6' size='4'>
-            <TinaClientContext.Provider value={client}>
-              {children}
-            </TinaClientContext.Provider>
+            {children}
           </Container>
         </Theme>
       </body>

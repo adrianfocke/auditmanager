b64d27163267af86e0cd745b71657fd082ee1b32
Focke Adrian <adrian.focke@extern.a1.at>
2024-03-18 12:45:41 +0100

    picker branch in config (oops)

--- tina/config.tsx
+++ tina/config.tsx
@@ -6,7 +6,8 @@ import files from './collections/files';
 import people from './collections/people';
 import standards from './collections/standards';
 
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main';
+const branch =
+  process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'picker';
 
 export default defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID ?? '',

bbf458a1a99aa3b5841bbd57527d2d5b04e00708
Focke Adrian <adrian.focke@extern.a1.at>
2024-03-20 22:11:45 +0100

    Revert "try using route handler to fetch directly"
    This reverts commit a41cd9c3ac710428e07b4092874ec3f99b16ca92.

--- tina/picker/Picker.tsx
+++ tina/picker/Picker.tsx
@@ -3,10 +3,9 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { CheckIcon } from '@radix-ui/react-icons';
 import { Card, Text } from '@radix-ui/themes';
 import { uniqueUuid } from 'docx';
-import React, { Suspense, use, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { wrapFieldsWithMeta } from 'tinacms';
 import tinaFetch from '../../app/actions/graphql/tinaFetch';
-import { POST } from '../../app/api/route';
 
 export default (type: PickerType) =>
   wrapFieldsWithMeta((props) => {
@@ -18,49 +17,41 @@ export default (type: PickerType) =>
       );
     }, [props]);
 
-    useEffect(() => {
-      Promise.resolve(POST())
-        .then((values) => values.json())
-        .then((data) => console.log('Data: , ', data));
-    }, [props]);
-
     if (!values) {
       return null;
     }
 
     return (
-      <Suspense>
-        <div key={uniqueUuid()}>
-          <Text className='font-sans text-xs font-semibold text-gray-700 whitespace-normal mb-2'>
-            {type}
-          </Text>
-          <Card className='h-fit max-h-64 overflow-y-scroll rounded-lg bg-tina-grey'>
-            {values.map((item: string, i) => (
-              <div key={uniqueUuid()}>
-                <Checkbox.Root
-                  onClick={() => {
-                    if (props.input.value.includes(item)) {
-                      const valuesWithoutCurrent = props.input.value.filter(
-                        (all: any) => all !== item
-                      );
-                      props.input.onChange(valuesWithoutCurrent);
-                    } else {
-                      props.input.onChange([...props.input.value, item]);
-                    }
-                  }}
-                  checked={props.input.value.includes(item)}
-                  className={`h-[18px] w-[18px] rounded border text-blue-500 focus:ring-indigo-500 transition ease-out duration-150 border-blue-500 shadow-sm mr-2`}
-                  id={`c-${i}`}
-                >
-                  <Checkbox.Indicator>
-                    <CheckIcon />
-                  </Checkbox.Indicator>
-                </Checkbox.Root>
-                <label htmlFor={`c-${i}`}>{item}</label>
-              </div>
-            ))}
-          </Card>
-        </div>
-      </Suspense>
+      <div key={uniqueUuid()}>
+        <Text className='font-sans text-xs font-semibold text-gray-700 whitespace-normal mb-2'>
+          {type}
+        </Text>
+        <Card className='h-fit max-h-64 overflow-y-scroll rounded-lg bg-tina-grey'>
+          {values.map((item: string, i) => (
+            <div key={uniqueUuid()}>
+              <Checkbox.Root
+                onClick={() => {
+                  if (props.input.value.includes(item)) {
+                    const valuesWithoutCurrent = props.input.value.filter(
+                      (all: any) => all !== item
+                    );
+                    props.input.onChange(valuesWithoutCurrent);
+                  } else {
+                    props.input.onChange([...props.input.value, item]);
+                  }
+                }}
+                checked={props.input.value.includes(item)}
+                className={`h-[18px] w-[18px] rounded border text-blue-500 focus:ring-indigo-500 transition ease-out duration-150 border-blue-500 shadow-sm mr-2`}
+                id={`c-${i}`}
+              >
+                <Checkbox.Indicator>
+                  <CheckIcon />
+                </Checkbox.Indicator>
+              </Checkbox.Root>
+              <label htmlFor={`c-${i}`}>{item}</label>
+            </div>
+          ))}
+        </Card>
+      </div>
     );
   });
